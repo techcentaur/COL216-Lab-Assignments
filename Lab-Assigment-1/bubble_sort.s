be1b534f25b402bdbc5cc9e1917164fc2ee2b134
Ankit Solanki <ankit03june@gmail.com>
2018-01-09 00:00:23 +0530

    bubble sort arm assembly code

--- Lab-Assigment-1/bubble_sort.s
+++ Lab-Assigment-1/bubble_sort.s
@@ -1,51 +1,50 @@
 	.equ SWI_Exit, 0x11
+	
+	.data
+AA:	.word 2,6,3,9,4,7
+	
 	.text
    	.globl _start
+
 _start:
-	mov r0,#4 			@ r0=4
-	ldr r1,[r2] 		@ first address ->r1
-	ldr r2,[r3]			@ no-of-numbers -> r2
-@	cmp r2,#1  			@ if r2<1
-@	ble _breakout		@ break
-	b _outerloop
+	ldr r0,=AA 			
+	
+	mov r2,#6
+	sub r2,r2,#1
+	
+	mov r8,#4
+	mul r1,r2,r8
+	mov r2,r1
 
-@_breakout:
-@	swi     0x11	@ invoke syscall
+_loop: 				
+	mov r3,#0	
 
-_outerloop:
-	mov r3,#0 			@ r3 => i=0
-	sub r2,r2,#1		@ r2 => n-1
-	b _innerloop
-	cmp r3,r2			@ for all <n-1
-	add r3,r3,#1
-	ble _outerloop
-
-_innerloop:
-	mov r4,#0			@ j=0
-	sub r5,r2,r3 		@ n-1-j
-	b _check
+_L:	ldr r4,[r0,r3]
+	add r7, r3,#4
+	ldr r5,[r0,r7]
+	
 	cmp r4,r5
-	add r4,r4,#1
-	ble _innerloop
-
-_check:
-	mul r6,r4,r0 		@r6 = the offset for the no.
-	ldr r7,[r1,r6]
-	ldr r8,[r7,#4]		@ r7,r8 are 2 numbers
-	cmp r7,r8			
-	bgt _swap			@ if r7 greater than r8
-	mov pc,lr
+	ble _cn
 
 _swap:
-	ldr r9,[r8]			@sawping values
-	str r7,[r8]
-	str r8,[r9]
-	mov pc,lr
+	str r4,[r0,r7]
+	str r5,[r0,r3]
+
+_cn:
+	add r3,r3,#4
+	cmp r3,r1
+	blt _L
+
+_ok:
+	sub r1,r1,#4
+	cmp r1,#0
+	bne _loop
 
 	swi SWI_Exit
 	.end
 
 
+# Bubble sort - 
 # void bsort(int arr, int n){
 # 	for(int i=0;i<n-1;i++){
 # 		for(int j=0,j<n-1-i;j++){
