464a99a2bb1f6139b37bfff9de35f9790d79d991
Ankit Solanki <ankit03june@gmail.com>
2018-04-25 23:55:43 +0530

    BRAM_wrapped.vhd introduction

---
+++ BRAM-files/BRAM_wrapper.vhd
@@ -0,0 +1,49 @@
+--Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+--Date        : Wed Mar  7 01:20:59 2018
+--Host        : DESKTOP-G7G3ODN running 64-bit major release  (build 9200)
+--Command     : generate_target BRAM_wrapper.bd
+--Design      : BRAM_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity BRAM_wrapper is
+  port (
+    BRAM_PORTA_0_addr : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    BRAM_PORTA_0_clk : in STD_LOGIC;
+    BRAM_PORTA_0_din : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    BRAM_PORTA_0_dout : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    BRAM_PORTA_0_en : in STD_LOGIC;
+    BRAM_PORTA_0_rst : in STD_LOGIC;
+    BRAM_PORTA_0_we : in STD_LOGIC_VECTOR ( 3 downto 0 )
+  );
+end BRAM_wrapper;
+
+architecture STRUCTURE of BRAM_wrapper is
+  component BRAM is
+  port (
+    BRAM_PORTA_0_addr : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    BRAM_PORTA_0_clk : in STD_LOGIC;
+    BRAM_PORTA_0_din : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    BRAM_PORTA_0_dout : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    BRAM_PORTA_0_en : in STD_LOGIC;
+    BRAM_PORTA_0_rst : in STD_LOGIC;
+    BRAM_PORTA_0_we : in STD_LOGIC_VECTOR ( 3 downto 0 )
+  );
+  end component BRAM;
+begin
+BRAM_i: component BRAM
+     port map (
+      BRAM_PORTA_0_addr(31 downto 0) => BRAM_PORTA_0_addr(31 downto 0),
+      BRAM_PORTA_0_clk => BRAM_PORTA_0_clk,
+      BRAM_PORTA_0_din(31 downto 0) => BRAM_PORTA_0_din(31 downto 0),
+      BRAM_PORTA_0_dout(31 downto 0) => BRAM_PORTA_0_dout(31 downto 0),
+      BRAM_PORTA_0_en => BRAM_PORTA_0_en,
+      BRAM_PORTA_0_rst => BRAM_PORTA_0_rst,
+      BRAM_PORTA_0_we(3 downto 0) => BRAM_PORTA_0_we(3 downto 0)
+    );
+end STRUCTURE;
