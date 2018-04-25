c8727bdf81b684626e4595224266c25528de3a1d
Ankit Solanki <ankit03june@gmail.com>
2018-04-25 23:56:34 +0530

    BRAM Module updated

---
+++ BRAM-files/BRAM/synth/BRAM.vhd
@@ -0,0 +1,80 @@
+--Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+--Date        : Mon Apr  9 17:25:00 2018
+--Host        : sumit-zenbook running 64-bit major release  (build 9200)
+--Command     : generate_target BRAM.bd
+--Design      : BRAM
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity BRAM is
+  port (
+    BRAM_PORTA_0_addr : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    BRAM_PORTA_0_clk : in STD_LOGIC;
+    BRAM_PORTA_0_din : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    BRAM_PORTA_0_dout : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    BRAM_PORTA_0_en : in STD_LOGIC;
+    BRAM_PORTA_0_rst : in STD_LOGIC;
+    BRAM_PORTA_0_we : in STD_LOGIC_VECTOR ( 3 downto 0 )
+  );
+  attribute CORE_GENERATION_INFO : string;
+  attribute CORE_GENERATION_INFO of BRAM : entity is "BRAM,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=BRAM,x_ipVersion=1.00.a,x_ipLanguage=VHDL,numBlks=1,numReposBlks=1,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,synth_mode=OOC_per_IP}";
+  attribute HW_HANDOFF : string;
+  attribute HW_HANDOFF of BRAM : entity is "BRAM.hwdef";
+end BRAM;
+
+architecture STRUCTURE of BRAM is
+  component BRAM_blk_mem_gen_0_0 is
+  port (
+    clka : in STD_LOGIC;
+    rsta : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    addra : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    rsta_busy : out STD_LOGIC
+  );
+  end component BRAM_blk_mem_gen_0_0;
+  signal BRAM_PORTA_0_1_ADDR : STD_LOGIC_VECTOR ( 31 downto 0 );
+  signal BRAM_PORTA_0_1_CLK : STD_LOGIC;
+  signal BRAM_PORTA_0_1_DIN : STD_LOGIC_VECTOR ( 31 downto 0 );
+  signal BRAM_PORTA_0_1_DOUT : STD_LOGIC_VECTOR ( 31 downto 0 );
+  signal BRAM_PORTA_0_1_EN : STD_LOGIC;
+  signal BRAM_PORTA_0_1_RST : STD_LOGIC;
+  signal BRAM_PORTA_0_1_WE : STD_LOGIC_VECTOR ( 3 downto 0 );
+  signal NLW_blk_mem_gen_0_rsta_busy_UNCONNECTED : STD_LOGIC;
+  attribute X_INTERFACE_INFO : string;
+  attribute X_INTERFACE_INFO of BRAM_PORTA_0_clk : signal is "xilinx.com:interface:bram:1.0 BRAM_PORTA_0 CLK";
+  attribute X_INTERFACE_INFO of BRAM_PORTA_0_en : signal is "xilinx.com:interface:bram:1.0 BRAM_PORTA_0 EN";
+  attribute X_INTERFACE_INFO of BRAM_PORTA_0_rst : signal is "xilinx.com:interface:bram:1.0 BRAM_PORTA_0 RST";
+  attribute X_INTERFACE_INFO of BRAM_PORTA_0_addr : signal is "xilinx.com:interface:bram:1.0 BRAM_PORTA_0 ADDR";
+  attribute X_INTERFACE_PARAMETER : string;
+  attribute X_INTERFACE_PARAMETER of BRAM_PORTA_0_addr : signal is "XIL_INTERFACENAME BRAM_PORTA_0, MASTER_TYPE BRAM_CTRL, MEM_ECC NONE, MEM_SIZE 8192, MEM_WIDTH 32, READ_WRITE_MODE READ_WRITE";
+  attribute X_INTERFACE_INFO of BRAM_PORTA_0_din : signal is "xilinx.com:interface:bram:1.0 BRAM_PORTA_0 DIN";
+  attribute X_INTERFACE_INFO of BRAM_PORTA_0_dout : signal is "xilinx.com:interface:bram:1.0 BRAM_PORTA_0 DOUT";
+  attribute X_INTERFACE_INFO of BRAM_PORTA_0_we : signal is "xilinx.com:interface:bram:1.0 BRAM_PORTA_0 WE";
+begin
+  BRAM_PORTA_0_1_ADDR(31 downto 0) <= BRAM_PORTA_0_addr(31 downto 0);
+  BRAM_PORTA_0_1_CLK <= BRAM_PORTA_0_clk;
+  BRAM_PORTA_0_1_DIN(31 downto 0) <= BRAM_PORTA_0_din(31 downto 0);
+  BRAM_PORTA_0_1_EN <= BRAM_PORTA_0_en;
+  BRAM_PORTA_0_1_RST <= BRAM_PORTA_0_rst;
+  BRAM_PORTA_0_1_WE(3 downto 0) <= BRAM_PORTA_0_we(3 downto 0);
+  BRAM_PORTA_0_dout(31 downto 0) <= BRAM_PORTA_0_1_DOUT(31 downto 0);
+blk_mem_gen_0: component BRAM_blk_mem_gen_0_0
+     port map (
+      addra(31 downto 0) => BRAM_PORTA_0_1_ADDR(31 downto 0),
+      clka => BRAM_PORTA_0_1_CLK,
+      dina(31 downto 0) => BRAM_PORTA_0_1_DIN(31 downto 0),
+      douta(31 downto 0) => BRAM_PORTA_0_1_DOUT(31 downto 0),
+      ena => BRAM_PORTA_0_1_EN,
+      rsta => BRAM_PORTA_0_1_RST,
+      rsta_busy => NLW_blk_mem_gen_0_rsta_busy_UNCONNECTED,
+      wea(3 downto 0) => BRAM_PORTA_0_1_WE(3 downto 0)
+    );
+end STRUCTURE;
